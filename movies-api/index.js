e8e7dce12363c615f53dfc43b2c2523f0b2e3892
YiCheng Cai <20091598@mail.wit.ie>
2021-01-02 19:35:32 +0000

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,6 +6,8 @@ import './db';
 import {loadUsers} from './seedData'
 import usersRouter from './api/users';
 import genresRouter from './api/genres'
+import session from 'express-session';
+import authenticate from './authenticate';
 
 dotenv.config();
 if (process.env.SEED_DB) {
@@ -24,8 +26,16 @@ const port = process.env.PORT;
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
+//session middleware
+app.use(session({
+    secret: 'ilikecake',
+    resave: true,
+    saveUninitialized: true
+  }));
+  
 app.use(express.static('public'));
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/genres', genresRouter)
 app.use(errHandler);
